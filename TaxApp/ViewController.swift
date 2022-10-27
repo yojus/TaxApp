facae0179584585061493544c7285ea86e361a17
velmouth-star <shanpuyangshu@gmail.com>
2022-10-27 14:58:50 +0900

    Initial Commit

---
+++ TaxApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaxApp
+//
+//  Created by 杉浦陽樹 on 2022/10/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
